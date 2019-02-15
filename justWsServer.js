97fa9ded5670774e388f7e30ba4aa8ca9551ec35
sbishop <sbishop@navarik.com>
2019-02-15 13:29:44 -0800

    Display both message data structure and string.

--- justWsServer.js
+++ justWsServer.js
@@ -14,7 +14,7 @@ wsServer.on('connection', (wsSocket, req) => {
   wsSocket.send('Run the server then open justWs.html. F12->Console and this string should be in the Message.')
   wsSocket.on('message', (msg) => {
     console.log(msg);
-})
+  })
 })
 
 httpServer.listen(8000)
